4fe85663482556b088e21304f9a3b8f10631f67b
Sebastien Hamel <sebhamel09@gmail.com>
2025-01-10 19:09:40 +0000

    + categories_listing_page mode

--- src/models/modes.py
+++ src/models/modes.py
@@ -2,6 +2,7 @@ from enum import Enum
 
 class Mode(str, Enum):
     CATEGORIES_LISTING = "categories_listing"
+    CATEGORIES_LISTING_PAGE = "categories_listing_page"
     RECIPE_LINKS = "recipe_links"
     RECIPE_DETAILS = "recipe_details"
 
