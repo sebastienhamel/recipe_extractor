0cf7aa49ab459eecb0062d6ac516039ee836b3d1
Sebastien Hamel <sebhamel09@gmail.com>
2025-01-14 00:30:52 +0000

    + scraping functions draft

--- src/detail_scraper.py
+++ src/detail_scraper.py
@@ -8,3 +8,29 @@ class RecipeScraper():
     def get_recipe_prep_time():
         pass
     
+    def get_ingredients():
+        pass
+
+    def get_ingredient_quantity():
+        pass 
+
+    def get_ingredient_name():
+        pass
+
+    def get_method():
+        pass
+
+    def get_portions():
+        pass
+
+    def get_cooking_time():
+        pass
+
+    def get_total_time():
+        pass
+
+    def get_category():
+        pass
+
+    def get_keywords():
+        pass
